668c11d0a91cafa5eb232ad623dc477ed9c7c39f
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-09-28 15:34:58 +0700

    update

--- application/modules/pembelian/views/cetak_pesanan_pembelian.php
+++ application/modules/pembelian/views/cetak_pesanan_pembelian.php
@@ -1,255 +1,135 @@
 <!DOCTYPE html>
 <html>
-	<head>
-	  <?= include 'lib.php'; ?>
-	  <title><?php echo $row['no_po'];?></title>
-	  
-	  <style type="text/css">
-	  	body{
-	  		font-family: "Open Sans", Arial, sans-serif;
-	  	}
-	  	table.minimalistBlack {
-		  border: 0px solid #000000;
-		  width: 98%;
-		  text-align: left;
-		}
-		table.minimalistBlack td, table.minimalistBlack th {
-		  border: 1px solid #000000;
-		  padding: 5px 4px;
-		}
-		table.minimalistBlack tr td {
-		  /*font-size: 13px;*/
-		}
-		table.minimalistBlack tr th {
-		  /*font-size: 14px;*/
-		  font-weight: bold;
-		  color: #000000;
-		  padding: 10px;
-		}
-		table tr.table-active{
-            background-color: #b5b5b5;
+    <head>
+      <title>BUKTI PENERIMAAN</title>
+      
+      <style type="text/css">
+        body{
+            font-family: "Open Sans", Arial, sans-serif;
         }
-        table tr.table-active2{
-            background-color: #cac8c8;
+        table tr.table-bold{
+           font-weight: bold;
         }
-		table tr.table-active3{
-            background-color: #eee;
-        }
-		hr{
-			margin-top:0;
-			margin-bottom:30px;
-		}
-		h3{
-			margin-top:0;
-		}
-	  </style>
+      </style>
 
-	</head>
-	<body>
-		<table width="98%" border="0" cellpadding="3">
-			<tr>
-				<td align="center">
-					<div style="display: block;font-weight: bold;font-size: 16px;">ORDER PEMBELIAN</div>
-				</td>
-			</tr>
-		</table>
-		<br /><br />
-		<table width="98%" border="0" cellpadding="3">
-			<tr>
-				<th width="20%">Nomor PO</th>
-				<th width="2%">:</th>
-				<th width="50%" align="left"><?php echo $row['no_po'];?></th>
-				<td align="left" width="28%">
-					Jakarta, <?= convertDateDBtoIndo($row["date_po"]); ?>
-				</td>
-			</tr>
-			<tr>
-				<th>Subjek</th>
-				<th width="10px">:</th>
-				<th align="left"><?php echo $row['subject'];?></th>
-			</tr>
-			<tr>
-				<th>Tgl PKP</th>
-				<th width="10px">:</th>
-				<th align="left">-</th>
-			</tr>
-			
-			<tr>
-				<th>NPWP</th>
-				<th width="10px">:</th>
-				<th align="left"><?php echo $row['npwp_supplier'];?></th>
-			</tr>
-			
-			<tr>
-				<td width="72%"></td>
-				<th width="28%" align="left"><b>Kepada Yth :</b></th>
-			</tr>
-			<tr>
-				<td width="72%"></td>
-				<th width="28%" align="left"><b><?php echo $row['supplier_name'];?></b></th>
-			</tr>
-			<tr>
-				<td width="72%"></td>
-				<th width="28%" align="left"><?php echo $row['address_supplier'];?></th>
-			</tr>
-		</table>
-		<br />
-		<br />
-		<table class="minimalistBlack" cellpadding="5" width="98%">
-			<tr class="table-active">
-                <th width="5%" align="center">No</th>
-                <th width="30%" align="center">Produk</th>
-                <th width="10%" align="center">Satuan</th>
-                <th width="15%" align="center">Volume</th>
-                <th width="15%" align="center">Harga</th>
-                <th width="25%" align="center">Subtotal</th>
+    </head>
+    <body>
+        <?
+        $prefix_title = explode(' ', $pembayaran['bayar_dari']);
+        ?>
+        <table width="98%" border="0" cellpadding="3">
+            <tr >
+                <td align="center">
+                    <div style=";font-weight: bold;font-size: 14px;border-bottom: 1px solid #000;border-top: 1px solid #000;">Bukti Pengeluaran <?= $bayar_dari;?></div>
+                    <div style="font-size: 10px;line-height: 20px"><?= $pembayaran['nomor_transaksi'];?></div>
+                </td>
             </tr>
-            <?php
-           $no=1;
-		   $subtotal = 0;
-		   $total = 0;
-		   $tax_0 = 0;
-		   $tax_ppn = 0;
-		   $tax_pph = 0;
-		   $tax_ppn11 = 0;
-           foreach ($details as $dt) {
-                $nilai = $dt['total'] * $dt['price'];
-               ?>   
-               <tr>
-                   <td align="center"><?php echo $no;?></td>
-                   <td align="center"><?php echo $this->crud_global->GetField('produk',array('id'=>$dt['material_id']),'nama_produk');?></td>
-                   <td align="center"><?php echo $dt['measure'];?></td>
-                   <td align="center"><?php echo number_format($dt['total'],2,',','.');?></td>
-                   <td align="right"><?php echo number_format($dt['price'],0,',','.');?></td>
-                   <td align="right"><?php echo number_format($nilai,0,',','.');?></td>
-               </tr>
-               <?php
-					$no++;
-					$subtotal += $dt['total'] * $dt['price'];
-					if($dt['tax_id'] == 4){
-						$tax_0 = true;
-					}
-					if($dt['tax_id'] == 3){
-						$tax_ppn += $dt['tax'];
-					}
-					if($dt['tax_id'] == 5){
-						$tax_pph += $dt['tax'];
-					}
-					if($dt['tax_id'] == 6){
-						$tax_ppn11 += $dt['tax'];
-					}
-				}
-				?>
-			<tr>
-               <th colspan="5" style="text-align:right">Sub Total</th>
-               <th align="right"><?= number_format($subtotal,0,',','.'); ?></th>
-           	</tr>
-			<?php
-            if($tax_ppn > 0){
-                ?>
-                <tr>
-                    <th colspan="5" align="right">Pajak (PPN 10%)</th>
-                    <th  align="right"><?= number_format($tax_ppn,0,',','.'); ?></th>
-                </tr>
-                <?php
-            }
-            ?>
-            <?php
-            if($tax_0){
-                ?>
-                <tr>
-                    <th colspan="5" align="right">Pajak (PPN 0%)</th>
-                    <th  align="right"><?= number_format(0,0,',','.'); ?></th>
-                </tr>
-                <?php
-            }
-            ?>
-            <?php
-            if($tax_pph > 0){
-                ?>
-                <tr>
-                    <th colspan="5" align="right">Pajak (PPh 23)</th>
-                    <th align="right"><?= number_format($tax_pph,0,',','.'); ?></th>
-                </tr>
-                <?php
-            }
-			?>
-			<?php
-            if($tax_ppn11 > 0){
-                ?>
-                <tr>
-                    <th colspan="5" align="right">Pajak (PPN 11%)</th>
-                    <th align="right"><?= number_format($tax_ppn11,0,',','.'); ?></th>
-                </tr>
-                <?php
-            }
-			
-            $total = $subtotal + $tax_ppn - $tax_pph + $tax_ppn11;
-            ?>
-            
+        </table>
+        <br /><br />
+        <table width="98%" border="0" cellpadding="3">
             <tr>
-                <th colspan="5" align="right">TOTAL</th>
-                <th align="right"><?= number_format($total,0,',','.'); ?></th>
+                <th width="25%">Dibayar Kepada</th>
+                <th width="2%">:</th>
+                <th width="73%" align="left"><?= $pembayaran['supplier_name'];?></th>
             </tr>
-		</table>
-		<p><b>Memo</b></p>
-		<p><?= $row["memo"] ?></p>
-		<br />
-		<br />
-		<br />
-		<br />
-		<?php
-        $ka_plant = $this->pmm_model->GetNameGroup(15);
-        ?> 
-		<table width="98%" border="0" cellpadding="0">
-			<tr >
-				<td width="5%"></td>
-				<td width="90%">
-					<table width="100%" border="1" cellpadding="2">
-						<tr class="table-active3">
-							<td align="center" >
-								<b><?php echo $row['supplier_name'];?></b>
-							</td>
-							<td align="center" >
-								<b>PT BIA BUMI JAYENDRA</b>
-							</td>
-						</tr>
-						<tr class="table-active3">
-							<td align="center" >
-								PENERIMA ORDER
-							</td>
-							<td align="center" >
-								PEMBERI ORDER
-							</td>
-						</tr>
-						<tr class="">
-							<td align="center" height="75px">
-								
-							</td>
-							<td align="center">
-								
-							</td>
-						</tr>
-						<tr class="table-active3">
-							<td align="center"><?php echo $row['pic'];?></td>
-							<td align="center"><?= $ka_plant['admin_name'];?></td>
-						</tr>
-						<tr class="table-active3">
-							<td align="center" >
-								<b><?php echo $row['position'];?></b>
-							</td>
-							<td align="center" >
-								<b>Ka. Unit Bisnis</b>
-							</td>
-						</tr>
-					</table>
-				</td>
-				<td width="5%"></td>
-			</tr>
-		</table>
-
-			
-		
-
-	</body>
+            <tr>
+                <th>Tanggal</th>
+                <th >:</th>
+                <th align="left"><?= date('d F Y',strtotime($pembayaran['tanggal_pembayaran']));?></th>
+            </tr>
+            <tr>
+                <th>Untuk Pembayaran</th>
+                <th >:</th>
+                <th align="left">Purchase Invoice <?= $this->crud_global->GetField('pmm_penagihan_pembelian',array('id'=>$pembayaran['penagihan_pembelian_id']),'nomor_invoice');?></th>
+            </tr>
+            <tr>
+                <th>Keterangan</th>
+                <th >:</th>
+                <th align="left"><?= $pembayaran['memo'];?></th>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <table width="98%" border="0" cellpadding="0">
+            <tr>
+                <th width="25%" ><div style="font-size:8px;font-weight: bold;line-height: 25px;border-bottom:1px solid #a0a0a0;border-top:1px solid #a0a0a0;text-align:center;">Jumlah</div></th>
+                <th width="25%" align="left">
+                    <div class="total" style="position:relative; background-color: #bf9b30;font-size:8px;font-weight: bold;line-height: 25px;border-bottom:1px solid #a0a0a0;border-top:1px solid #a0a0a0;">
+                       
+                        Rp. <?= number_format($pembayaran['total'],0,',','.'); ?>
+                    </div>
+                </th>
+            </tr>
+            <tr >
+                <th width="25%" ><div style="font-size:8px;font-weight: bold;line-height: 0px;text-align: center;">Terbilang</div></th>
+				<th width="50%" ><div style="font-size:8px;font-style: italic;font-weight: bold;line-height: 0px;text-align: left;text-transform:capitalize;">: <?= $this->filter->terbilang($pembayaran['total']);?></div></th>
+                
+            </tr>
+        </table>
+        <br />
+        <br />
+        <table width="98%" border="0" cellpadding="30">
+            <tr >
+                <td width="5%"></td>
+                <td width="90%">
+                    <table width="100%" border="1" cellpadding="3">
+                        <tr class="">
+                            <td align="center" >
+                                Dibuat Oleh
+                            </td>
+                            <td align="center" >
+                                Diperiksa dan Disetujui Oleh
+                            </td>
+                            <td align="center" >
+                                Diketahui Oleh
+                            </td>
+                        </tr>
+                        <tr class="">
+                            <td align="center" height="75px">
+                                
+                            </td>
+                            <td align="center">
+                                
+                            </td>
+                            <td align="center">
+                                
+                            </td>
+                        </tr>
+                        <tr class="table-bold">
+                            <td align="center" >
+                                <?= $this->crud_global->GetField('tbl_admin',array('admin_id'=>$pembayaran['created_by']),'admin_name'); ?>
+                            </td>
+                            <td align="center" >
+                                Erika Sinaga
+                            </td>
+                            <td align="center" >
+                                Deddy Sarwobiso
+                            </td>
+                        </tr>
+                        <tr class="table-bold">
+                            <td align="center" >
+                            <?php
+                            $this->db->select('g.admin_group_name');
+                            $this->db->join('tbl_admin_group g','a.admin_group_id = g.admin_group_id','left');
+                            $this->db->where('a.admin_id',$pembayaran['created_by']);
+                            $created_group = $this->db->get('tbl_admin a')->row_array();
+                            ?>
+                                <b><?= $created_group['admin_group_name']?></b>
+                            </td>
+                            <td align="center" >
+                                <b>Dir. Keuangan</b>
+                            </td>
+                            <td align="center" >
+                                <b>Direktur Utama</b>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td width="5%"></td>
+            </tr>
+        </table>
+    </body>
 </html>
